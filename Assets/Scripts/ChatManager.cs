2cef3cc9f35d8e6e8f284e01eaf6eff61922e989
kisa002 <vnycall74@naver.com>
2017-12-02 21:18:15 +0900

    UCHAT

---
+++ Assets/Scripts/ChatManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ChatManager : NetworkBehaviour {
+
+    Text textChat;
+    public Text inputMessage;
+
+    Button buttonSend;
+
+    [SyncVar]
+    public string msg;
+    
+	void Start ()
+    {
+        Init();
+	}
+
+    void Init()
+    {
+        textChat = GameObject.Find("TextChat").GetComponent<Text>();
+        inputMessage = GameObject.Find("InputMessage").transform.FindChild("Text").GetComponent<Text>();
+
+        buttonSend = GameObject.Find("ButtonSend").GetComponent<Button>();
+
+        //if (isLocalPlayer)
+            buttonSend.onClick.AddListener(delegate { MessageSend(); });
+    }
+	
+	void Update ()
+    {
+        MessageAdd();
+    }
+
+    void MessageAdd()
+    {
+        if (msg != null)
+        {
+            textChat.text += msg;
+
+            msg = null;
+            inputMessage.text = "";
+        }
+    }
+
+    public void MessageSend()
+    {
+        CmdMessageSend();
+        RpcMessageSend();
+    }
+
+    [Command]
+    public void CmdMessageSend()
+    {
+        msg = inputMessage.text + "\n";
+    }
+
+    [ClientRpc]
+    public void RpcMessageSend()
+    {
+        msg = inputMessage.text + "\n";
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public TextMesh textName;
+
+    [SyncVar]
+    public int a = 1;
+
+	// Use this for initialization
+	void Start () {
+        textName = this.transform.FindChild("TextName").GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 20f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 20f;
+
+        transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            CmdAddValue();
+        }
+
+        textName.text = a.ToString();
+
+        CmdSetText();
+        RpcSetText();
+	}
+
+    [Command]
+    public void CmdAddValue()
+    {
+        a += 1;
+
+        Debug.Log(a);
+    }
+
+    [Command]
+    public void CmdSetText()
+    {
+        textName.text = a.ToString();
+    }
+
+    [ClientRpc]
+    public void RpcSetText()
+    {
+        textName.text = a.ToString();
+    }
+}
